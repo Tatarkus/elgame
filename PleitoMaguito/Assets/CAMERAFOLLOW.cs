518f2547d71e1ad24f9d58a9ec10ac42c3e2b2f9
D4nk0 <danko.moyla.lohaus@gmail.com>
2020-07-10 15:04:23 -0400

    Cámara
    It follows you (?

---
+++ PleitoMaguito/Assets/CAMERAFOLLOW.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CAMERAFOLLOW : MonoBehaviour {
+    
+    public Transform Player;
+    public float dampTime = 0.4f;
+    private Vector3 cameraPos;
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate ()
+    {
+        cameraPos = new Vector3(Player.position.x, Player.position.y, -10f);
+        transform.position = Vector3.SmoothDamp(gameObject.transform.position, cameraPos, ref velocity, dampTime);
+    }
+}
